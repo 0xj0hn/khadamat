4311c8c98dafaa6d374b75230eae92779f15fb49
John Peterson <nxh7@duck.com>
2022-12-01 15:24:04 +0330

    Add fake data to test app

---
+++ lib/utils/api/fake_data.dart
@@ -0,0 +1,12 @@
+class FakeData {
+  static final ticket = {
+    "id": 1,
+    "priority": 1,
+    "subject": "ارزش افزوده",
+    "title": "مشکل فلان",
+    "description": "این یک متن بلند می‌باشد که شما باید فلان ...",
+    "submitter": 9,
+    "status": 1,
+    "answers": [],
+  };
+}
