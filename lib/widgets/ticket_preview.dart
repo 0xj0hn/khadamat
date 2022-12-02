d99797bd8829cc606cfd6485b1f9a8c39bd53439
John Peterson <nxh7@duck.com>
2022-12-02 10:35:19 +0330

    Edit ticket preview

--- lib/widgets/ticket_preview.dart
+++ lib/widgets/ticket_preview.dart
@@ -22,7 +22,7 @@ class TicketPreview extends StatelessWidget {
             Navigator.of(context).push(
               GetPageRoute(
                 page: () => TicketScreen(
-                  title: ticket.title,
+                  ticket: ticket,
                 ),
               ),
             );
