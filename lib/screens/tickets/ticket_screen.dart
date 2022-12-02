af636c59700f501d1d1226f789d7b2b0a20e6145
John Peterson <nxh7@duck.com>
2022-12-02 22:28:45 +0330

    Update ticket screen as well

--- lib/screens/tickets/ticket_screen.dart
+++ lib/screens/tickets/ticket_screen.dart
@@ -1,12 +1,25 @@
 import 'package:TexBan/utils/models/ticket_model.dart';
+import 'package:TexBan/utils/models/ticket_screen_model.dart';
+import 'package:TexBan/utils/models/tickets_detail_screen_state_model.dart';
 import 'package:TexBan/widgets/appbar.dart';
 import 'package:TexBan/widgets/custom_bubble.dart';
 import 'package:TexBan/widgets/ticket_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 class TicketScreen extends StatelessWidget {
   Ticket ticket;
   TicketScreen({super.key, required this.ticket});
+  TextEditingController textController = TextEditingController();
+  TicketScreenModel ticketScreenModel = Get.find();
+  TicketsDetailScreenStateModel ticketsDetailModel = Get.find();
+  updateChatsAndTickets() async {
+    await ticketsDetailModel.fetchTickets();
+    List<dynamic> answers =
+        await ticketsDetailModel.updateAndChangeSpecificTicket(ticket.id);
+    ticketScreenModel.getAnswersAndUpdateChatBubbles(answers);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +27,42 @@ class TicketScreen extends StatelessWidget {
       appBar: CustomedAppBar(title: ticket.title),
       body: Stack(
         children: [
-          ListView(
-            reverse: true,
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              CustomBubble(
-                text: ticket.description,
-                type: "sendBubble",
-                time: DateTime.now().toIso8601String(),
-              ),
-            ],
+          GetBuilder<TicketScreenModel>(
+            init: TicketScreenModel(),
+            didChangeDependencies: (state) async {
+              List answers = ticket.answers;
+              await state.controller!.getAnswersAndUpdateChatBubbles(answers);
+            },
+            builder: (model) {
+              return RefreshIndicator(
+                onRefresh: () async {
+                  updateChatsAndTickets();
+                },
+                child: ListView(
+                  reverse: true,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    if (model.isChatsLoading)
+                      Container(
+                        alignment: Alignment.bottomCenter,
+                        height: 20,
+                        width: 20,
+                        child: LoadingIndicator(
+                          indicatorType: Indicator.ballBeat,
+                        ),
+                      ),
+                    ...model.chatBubbles.reversed,
+                    CustomBubble(
+                      text: ticket.description,
+                      type: "sendBubble",
+                      time: DateTime.now().toIso8601String(),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
           Align(
             alignment: Alignment.bottomCenter,
@@ -39,15 +76,29 @@ class TicketScreen extends StatelessWidget {
                     Expanded(
                       flex: 8,
                       child: TextField(
+                        controller: textController,
                         maxLines: null,
                         textInputAction: TextInputAction.newline,
-                        decoration: InputDecoration(),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "پیامی وارد کنید...",
+                        ),
                       ),
                     ),
                     Expanded(
                       flex: 1,
                       child: IconButton(
-                        onPressed: () {},
+                        onPressed: () async {
+                          if (textController.text.isEmpty) return;
+
+                          ticketScreenModel.userProvider.ticketSection
+                              .sendAnswer(
+                            ticket.id,
+                            textController.text,
+                          );
+                          updateChatsAndTickets();
+                          textController.text = "";
+                        },
                         icon: Icon(
                           Icons.send,
                         ),
