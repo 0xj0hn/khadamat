12563a211c656afc1918069e3f882c92545bcd86
John Peterson <nxh7@duck.com>
2022-12-02 10:34:02 +0330

    Add Ticket screen

--- lib/screens/tickets/ticket_screen.dart
+++ lib/screens/tickets/ticket_screen.dart
@@ -1,16 +1,64 @@
+import 'package:TexBan/utils/models/ticket_model.dart';
 import 'package:TexBan/widgets/appbar.dart';
+import 'package:TexBan/widgets/custom_bubble.dart';
+import 'package:TexBan/widgets/ticket_preview.dart';
 import 'package:flutter/material.dart';
 
 class TicketScreen extends StatelessWidget {
-  String title;
-  TicketScreen({super.key, required this.title});
+  Ticket ticket;
+  TicketScreen({super.key, required this.ticket});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomedAppBar(title: title),
-      body: Center(
-        child: Text("hello"),
+      appBar: CustomedAppBar(title: ticket.title),
+      body: Stack(
+        children: [
+          ListView(
+            reverse: true,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              CustomBubble(
+                text: ticket.description,
+                type: "sendBubble",
+                time: DateTime.now().toIso8601String(),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              height: 200,
+              child: Container(
+                color: Theme.of(context).canvasColor,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 8,
+                      child: TextField(
+                        maxLines: null,
+                        textInputAction: TextInputAction.newline,
+                        decoration: InputDecoration(),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.send,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
